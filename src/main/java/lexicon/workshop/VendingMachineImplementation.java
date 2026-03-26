8ac5e94590f82f05e30e3e1f95b8c27135d33da0
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-26 13:55:43 +0100

    Implemented the interface methods in implementation class

--- src/main/java/lexicon/workshop/VendingMachineImplementation.java
+++ src/main/java/lexicon/workshop/VendingMachineImplementation.java
@@ -1,5 +1,31 @@
 package lexicon.workshop;
 
-public class VendingMachineImplementation {
+import java.util.List;
 
+public class VendingMachineImplementation implements IVendingMachine {
+
+    @Override
+    public void insertCoin(int coin) {
+
+    }
+
+    @Override
+    public int getBalance() {
+        return 0;
+    }
+
+    @Override
+    public Product purchaseProduct(int productId) {
+        return null;
+    }
+
+    @Override
+    public int returnChange() {
+        return 0;
+    }
+
+    @Override
+    public List<Product> getProducts() {
+        return List.of();
+    }
 }
