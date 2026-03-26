11f5b873a91a54a537ff14974b776ec97c32c372
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-26 13:53:46 +0100

    added method definitions in interface

--- src/main/java/lexicon/workshop/IVendingMachine.java
+++ src/main/java/lexicon/workshop/IVendingMachine.java
@@ -1,5 +1,17 @@
 package lexicon.workshop;
 
+import java.util.List;
+
 public interface IVendingMachine {
+
+    public void insertCoin(int coin);
+
+    public int getBalance();
+
+    public Product purchaseProduct(int productId);
+
+    public int returnChange();
+
+    public List<Product> getProducts();
 
 }
